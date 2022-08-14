f5f6069d1e5e525965774c5f7e7295c425fdd307
Lohana Torres <lt.leopoldo@gmail.com>
2022-08-14 15:38:49 -0300

    feat: dynamically listing posts

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,44 @@
 import { Header } from "./components/Header";
 import { Post } from "./components/Post";
+import { SideBar } from "./components/SideBar";
 
 import styles from './App.module.css';
 
 import './global.css';
-import { SideBar } from "./components/SideBar";
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/heyloh.png",
+      name: "Lohana Torres",
+      role: "Software Developer @ Serasa Experian"
+    },
+    publishedAt: new Date('2022-07-07 20:00:00'),
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portfólio. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: '👉 heyloh.design/doctorcare' },
+      { type: 'link', content: '#novoprojeto' },
+      { type: 'link', content: '#nlw' },
+      { type: 'link', content: '#rocketseat' },
+    ]
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://github.com/heyloh.png",
+      name: "Tauriel",
+      role: "Hunter @ Home"
+    },
+    publishedAt: new Date('2022-07-07 20:00:00'),
+    content: [
+      { type: 'paragraph', content: 'Miau' },
+      { type: 'paragraph', content: 'Miau Miau 🚀' },
+      { type: 'link', content: '#miau' },
+    ]
+  }
+];
 
 export function App() {
   return (
@@ -14,7 +48,14 @@ export function App() {
       <div className={styles.wrapper}>
         <SideBar />
         <main>
-          <Post />
+          {posts.map(post => (
+            <Post
+              key={post.id}
+              author={post.author}
+              publishedAt={post.publishedAt}
+              content={post.content}
+            />
+          ))}
         </main>
       </div>
     </>
